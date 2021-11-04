a68fd989d6565438426e603e561165afea7535fc
Selupucin <73664118+Selupucin@users.noreply.github.com>
2021-11-04 13:08:00 +0300

    Index new version 0,1
    Add team, plays and bars area html,css

--- public_html/assets/js/main.js
+++ public_html/assets/js/main.js
@@ -3,7 +3,9 @@ $(window).scroll(function () {
     var height = $(window).scrollTop();
     if (height > 50) {
         $('header').addClass('sticky-header');
+        $('#site-logo').addClass('site-logo-active')
     } else {
         $('header').removeClass('sticky-header');
+        $('#site-logo').removeClass('site-logo-active')
     }
 });
